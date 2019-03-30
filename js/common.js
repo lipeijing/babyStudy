09c219d547cd515d1710625992c4b10f146cadb4
lee_scs@hotmail <lee_scs@hotmail>
2019-03-30 16:04:18 +0900

    #と?両方対応

--- js/common.js
+++ js/common.js
@@ -1,7 +1,14 @@
 function GetQueryString() {
-	if (location.hash.length > 1) {
+	if (location.hash.length > 1 || location.search.length > 1) {
 		// 最初の1文字 (?記号) を除いた文字列を取得する
-		var query = location.hash.substring(1);
+		var query;
+		if (location.hash.length > 1) {
+			// #param=valueの場合
+			query = location.hash.substring(1);
+		} else {
+			// ?param=valueの場合
+			query = location.search.substring(1);
+		}
 
 		// クエリの区切り記号 (&) で文字列を配列に分割する
 		var parameters = query.split('&');
